4d9c237af0c71318c0c20be68416163d2c7e137c
gmoe1994 <thomasgansmoe@hotmail.com>
2022-04-27 18:04:23 +0200

    v.0.1 ready for XAML

---
+++ CryptoTrader.App.Core/ApiAdress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace CryptoTrader.App.Core
+{
+    public static class ApiAdress
+    {
+        public const string DataApi = "https://api.nomics.com/v1/currencies/ticker";
+
+        private static readonly string API_KEY = "a9dd48ab86d9a5226102aa57b06c0502fbf4259f";
+
+        public static string GetApiAdress()
+        {
+            var URL = new UriBuilder(DataApi);
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["key"] = API_KEY;
+            queryString["interval"] = "1d";
+            queryString["convert"] = "USD";
+            queryString["per-page"] = "100";
+            queryString["page"] = "1";
+
+            URL.Query = queryString.ToString();
+
+            return URL.ToString();
+        }
+    }
+}
--- CryptoTrader.App.Core/Services/ExternalApiService.cs
+++ CryptoTrader.App.Core/Services/ExternalApiService.cs
@@ -12,31 +12,13 @@ namespace CryptoTrader.App.Core.Services
 {
     public class ExternalApiService
     {
-        private static readonly string API_KEY = "a9dd48ab86d9a5226102aa57b06c0502fbf4259f";
-
-        public ExternalApiService()
-        {
-        }
+        public ExternalApiService() { }
 
         
         public async Task<IEnumerable<CryptoDto>> GetCryptoDtosAsync()
         {
-
-            var URL = new UriBuilder("https://api.nomics.com/v1/currencies/ticker");
-
-            var queryString = HttpUtility.ParseQueryString(string.Empty);
-            queryString["key"] = API_KEY;
-            //queryString["ids"] = "BTC,ETH,XRP,SOL,ADA,DOT,DOGE,SHIB,NEAR,XMR";
-            queryString["interval"] = "1d";
-            queryString["convert"] = "USD";
-            queryString["per-page"] = "100";
-            queryString["page"] = "1";
-
-            URL.Query = queryString.ToString();
-
             var client = new WebClient();
-            client.Headers.Add("Accepts", "application/json");
-            var itemsInString = await client.DownloadStringTaskAsync(URL.ToString());
+            var itemsInString = await client.DownloadStringTaskAsync(ApiAdress.GetApiAdress());
             var items = await Json.ToObjectAsync<List<CryptoDto>>(itemsInString);
 
             return items;
